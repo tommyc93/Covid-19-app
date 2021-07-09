03cf51d33083ff20f34fdcfa6cb4adfccd511a47
Tommy Chung <tommychung@TC-MacBook-Pro.local>
2021-07-09 16:26:54 -0400

    Day four MVP done

--- app.js
+++ app.js
@@ -26,29 +26,51 @@ $(() => {
     })
 
     // Modal Content
+
+
     $(".north_america, .south_america, .europe, .africa, .asia, .australia, .antarctica").on('click', (event) => {
+      event.preventDefault()
+      $('ul').remove()
 
       const $click = $(event.currentTarget).val()
+      console.log($click)
 
     $.ajax({
       url: "https://covid-api.mmediagroup.fr/v1/cases?continent=" + $click,
       type: "GET",
-      data:
-      {
-         country: 'All'
-      }
 
     }).then(
         (data) => {
             console.log(data)
             // Creating a loop for Dataset
-            for(let i = 0; i < data.length; i++) {
-                const $country = $('<dt>')
-                .addClass('country')
-                .text(data[i])
-                .css("list-style", "none")
-                appendTo('dl')
+            for(const dataset in data) {
+              const $country = $('<ul>')
+              .text(dataset)
+              .addClass('countries')
+              .appendTo('.country')
+
+              const $population = $('<li>')
+              .text(`Population size: ${data[dataset].All.population}`)
+              .appendTo($country)
+
+              const $confirmed = $('<li>')
+              .text(`Confirmed cases: ${data[dataset].All.confirmed}`)
+              .appendTo($country)
+
+              const $recovered = $('<li>')
+              .text(`Recovered: ${data[dataset].All.recovered}`)
+              .appendTo($country)
+
+              const $deaths = $('<li>')
+              .text(`Deaths: ${data[dataset].All.deaths}`)
+              .appendTo($country)
+
+              const $life_expectancy = $('<li>')
+              .text(`Average Life Expectancy: ${data[dataset].All.life_expectancy}`)
+              .appendTo($country)
+
             }
+
         },
         () => {
             console.log('bad request')
